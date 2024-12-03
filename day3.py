a96b502e4a5c8d32c4ddf38dbbb0ddb3630fdd13
Will Marshall <willcodymarshall@gmail.com>
2024-12-03 03:03:07 -0500

    day 3 pt 2

--- day3.py
+++ day3.py
@@ -2,9 +2,24 @@ import re
 
 import util
 
+MUL_RE = r"(?P<mul>mul)\((?P<a>\d{1,3}),(?P<b>\d{1,3})\)"
+DO_RE = r"(?P<do>do)\(\)"
+DONT_RE = r"(?P<dont>don't)\(\)"
+COMBINED_RE = rf"(?:{MUL_RE})|(?:{DO_RE})|(?:{DONT_RE})"
 
-def find_valid_muls(mem: str) -> list[tuple[int, int]]:
-    return [(int(a), int(b)) for a, b in re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", mem)]
+
+def find_valid_muls(mem: str, conditional_parse=True) -> list[tuple[int, int]]:
+    valid_muls = []
+    parsing_enabled = True
+    for instr in re.finditer(COMBINED_RE, mem):
+        match instr.groupdict():
+            case {"do": "do"} if conditional_parse:
+                parsing_enabled = True
+            case {"dont": "don't"} if conditional_parse:
+                parsing_enabled = False
+            case {"mul": "mul", "a": a, "b": b} if parsing_enabled:
+                valid_muls.append((int(a), int(b)))
+    return valid_muls
 
 
 def main():
